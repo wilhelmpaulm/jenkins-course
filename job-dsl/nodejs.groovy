c5b4d4e8531a8a585dfbd3dd9b7e6b9ebba41baa
wilhelm martinez <wilhelmpaulm@gmail.com>
2020-04-19 23:36:03 +0800

    update creds

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/wilhelmpaulm/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('wilhelm martinez')
+            node / gitConfigEmail('wilhempaulm@gmail.com')
         }
     }
     triggers {
